b96fe7046c40b5421a698fb4fb03ac578a3fc58f
Gaurav Bhatt <gauravkapadibhatt@gmail.com>
2025-04-16 14:54:39 +0545

    HTMX works

--- rtchat/views.py
+++ rtchat/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.views import View
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect,HttpResponse
 from .models import *
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .forms import ChatmessageCreateForm
 from django.shortcuts import redirect
+from django.template.loader import render_to_string
 
 class ChatView(LoginRequiredMixin,View):
     chat_group=ChatGroup.objects.get(group_name="public-chat")
@@ -18,7 +19,12 @@ class ChatView(LoginRequiredMixin,View):
     def get(self,request):
         return render(request,'rtchat/chat.html',self.context,)
     
-    def post(self,request):
+    def dispatch(self, request, *args, **kwargs):
+        if request.method.lower() == 'post' and request.headers.get('HX-Request'):
+            return self.htmx(request, *args, **kwargs)
+        return super().dispatch(request, *args, **kwargs)
+
+    def htmx(self,request):
         form = ChatmessageCreateForm(request.POST)
         if form.is_valid:
             message=form.save(commit=False)
@@ -26,4 +32,10 @@ class ChatView(LoginRequiredMixin,View):
             message.body = form.cleaned_data['body']
             message.group=self.chat_group
             message.save()
-            return redirect('chat')
+            context={
+                'message':message,
+                'user':request.user
+            }
+            html = render_to_string('rtchat/chat_message_partial.html',context,request=request)
+            return HttpResponse(html)
+        return HttpResponse("Form is invalid",status=400)
